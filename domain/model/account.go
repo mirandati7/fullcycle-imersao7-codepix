0e178d9d16c9592873933117e68bc927ccd6e9cd
mirandati7 <mirandati719@gmail.com>
2022-04-11 22:05:44 +0000

    Imersao 7 Full Stack Primeira aula GO domain-models

---
+++ domain/model/account.go
@@ -0,0 +1,44 @@
+package model
+
+import (
+	"time"
+	"github.com/asaskevich/govalidator"
+	uuid "github.com/satori/go.uuid"
+)
+
+func init() {
+	govalidator.SetFieldsRequiredByDefault(true)
+}
+
+type Account struct {
+	Base      `valid:"required"`
+	OwnerName string    `valid:"notnull"`
+	Bank      *Bank     `valid:"-"`
+	Number    string    `json:"number"  valid:"notnull"`
+	PixKeys []*PixKey  `valid:"-"`
+}
+
+func (account *Account) isValid() error {
+	_, err := govalidator.ValidateStruct(account)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func NewAccount(bank *Bank, number string, ownerName string) (*Account, error) {
+	account := Account{
+		OwnerName: ownerName,
+		Bank:      bank,
+		Number:    number,		
+	}
+
+	account.ID = uuid.NewV4().String()
+	account.CreatedAt = time.Now()
+
+	err := account.isValid()
+	if err != nil {
+		return nil, err
+	}
+	return &account, nil
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/mirandati7/fullcycle-imersao7-codepix
+
+go 1.15
